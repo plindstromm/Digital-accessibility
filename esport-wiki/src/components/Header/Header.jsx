36d55c37765cf68f6fa3f9c40f00ce95fb99d860
plindstromm <mukkepp@gmail.com>
2023-11-22 15:45:00 +0100

    header css

--- esport-wiki/src/components/Header/Header.jsx
+++ esport-wiki/src/components/Header/Header.jsx
@@ -3,7 +3,13 @@ import './Header.css'
 function Header() {
 
 return(
-    <section className='header'><h1>ES-wiki</h1></section>
+    <section 
+        className='header'>
+            <h1>ES-wiki</h1>
+            <h2 className="header-text">Profile</h2>
+            <h2 className="header-text">Contact us</h2>
+            <h2 className="header-text">About us</h2>
+    </section>
 )
 
 }
